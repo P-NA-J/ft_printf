d26700317bf7b8e666061583ab85e94f784cdbf6
Paul JULLIEN <paul.jullien.code@gmail.com>
2019-05-25 14:36:42 +0200

    new version ft_printf.c

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pauljull <pauljull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 21:56:41 by pauljull          #+#    #+#             */
-/*   Updated: 2019/04/27 15:41:09 by pauljull         ###   ########.fr       */
+/*   Updated: 2019/05/25 14:35:01 by pauljull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include "../include/ft_printf.h"
 #include "../libft/libft.h"
 
-int					bypass_flag(const char *restrict format)
+int					bypass_flag(const char *format)
 {
 	int i;
 
@@ -32,36 +32,80 @@ int					bypass_flag(const char *restrict format)
 	return (i);
 }
 
-unsigned long		print_final(t_plist *list, const char *restrict format)
-{
-	unsigned long	count;
 
+
+
+
+
+char				*ft_strnpjoinf(char *s1, char *s2, size_t n1, size_t n2)
+{
+	char	*new_str;
+
+	if (s1 == NULL)
+	{
+		if (!(new_str = ft_strndup(s2, n2)))
+			return (NULL);
+	}
+	if (!(new_str = (char *)malloc(sizeof(char) * (n1 + n2 + 1))))
+		return (NULL);
+	ft_memmove(new_str, s1, n1);
+	ft_memmove(new_str + n1, s2, n2);
+	new_str[n1 + n2] = 0;
+	free(s1);
+	return (new_str);
+}
+
+static char			*substitute(char *s_res, t_plist *list, size_t *len, size_t count)
+{
+	*len += list->vlc;
+	s_res = ft_strnpjoinf(s_res, list->tab, count, list->vlc);
+	return (s_res);
+}
+
+void				ft_putnstr(char *s, size_t len)
+{
+	write(1, s, len);
+}
+
+unsigned long		print_final(t_plist *list, const char *format)
+{
+	size_t	len;
+	size_t	count;
+	char	*s_res;
+	char	*ps_f;
+
+	len = 0;
 	count = 0;
+	ps_f = (char *)format;
+	s_res = NULL;
 	while (*format)
 	{
-		if (*format == '%' && *(format + 1) != '%')
+		if (*format == '%' && *(format + 1) == '%')
 		{
-//			ft_putstr(list->tab);
-			printf("%s", list->tab);
-			format += bypass_flag(format);
-			count += ft_strlen(list->tab) - 1;
-			list = list->next;
+			s_res = ft_strnpjoinf(s_res, ps_f, count, format - ps_f);
+			s_res = ft_strnpjoinf(s_res, "%", count, 1);
+			format += 3;
+			count += 1;
+			ps_f = (char *)(format);
 		}
-		else if (*format == '%' && *(format + 1) == '%')
+		else if (*format == '%' && *(format + 1) != '%')
 		{
+			format += bypass_flag(format);
+			s_res = ft_strnpjoinf(s_res, ps_f, count, format - ps_f);
+			s_res = substitute(s_res, list, &len, count);
 			format += 1;
-//			ft_putstr("%");
-			printf("%%");
+			list = list->next;
+			ps_f = (char *)(format);
 		}
 		else
 		{
-			printf("%c", *format);
-//			ft_putchar(*format);
+			format++;
+			count += 1;
 		}
-		format += 1;
-		count += 1;
 	}
-	return (count);
+	s_res = ft_strnpjoinf(s_res, ps_f, count, format - ps_f);
+	ft_putnstr(s_res, count + len);
+	return (count + len);
 }
 
 int					tab_gen(t_plist *list, va_list arg)
@@ -71,6 +115,12 @@ int					tab_gen(t_plist *list, va_list arg)
 	if (list->flag & (1 << 5) || list->flag & (1 << 6)
 	|| list->flag & (1 << 7) || list->flag & (1 << 8))
 		return (unsigned_u_o_x_bigx(list, arg));
+	if (list->flag & C_FLAG)
+		return (char_conversion(list, arg));
+	if (list->flag & S_FLAG)
+		return (string_conversion(list, arg));
+	if (list->flag & P_FLAG)
+		return (p_conversion(list, arg));
 	return (1);
 }
 
@@ -107,18 +157,21 @@ int					ft_printf(const char *restrict format, ...)
 	t_plist			*list;
 	va_list			arg_list;
 	unsigned long	ret;
+	int				count;
 
 	list = NULL;
 	ret = 0;
+	count = 0;
 	va_start(arg_list, format);
 	if (!format)
 		return (-1);
-	if (!(parsing(format, &list)))
+	if (!(parsing(format, &list, &count)))
 		return (-1);
 	if (!fill_list(list, arg_list))
 		return (-1);
 	va_end(arg_list);
-	ret = print_final(list, format);
+	ret = print_final(list, (const char *)format);
+	//printf("jokerini\n");
 	free_list(list);
 	return (ret);
 }
