e764894982965f17961b1a3d5ffa61422415c13f
Paul JULLIEN <paul.jullien.code@gmail.com>
2019-06-03 15:38:47 +0200

    New ft_div_str.c

--- src_test/test.c
+++ src_test/test.c
@@ -1,164 +1,70 @@
 #include <stdio.h>
-#include "../include/ft_printf.h"
-#include "../libft/libft.h"
-#include <limits.h>
 #include <stdlib.h>
-#include <float.h>
 #include <string.h>
+#include "../libft/libft.h"
 
-int		nb_digit_f(unsigned long div)
+int		is_number(char *str)
 {
-	int i;
-
-	i = 0;
-	while (div)
+	while (*str)
 	{
-		i += 1;
-		div /= 10;
-	}
-	return (i);
-}
-
-char	*div_compute(char s_temp[100], unsigned long div)
-{
-	int	res;
-	char	tmp[100];
-
-	res = ft_atoi(s_temp) / div;
-	ft_bzero(tmp, 100);
-	if (!div)
-		return ("0");
-	ft_strcpy(tmp, s_temp);
-	printf("s_temp avant = |%s|\n", s_temp);
-	ft_bzero(s_temp, 100);
-	ft_strcpy(s_temp, ft_itoa(ft_atoi(tmp) - res * div));
-	printf("s_temp apres = |%s|\n", s_temp);
-	printf("DIVISION = %d\nRESTE = %lu\n\n", res, ft_atoi(tmp) - res * div);
-	return (ft_itoa(res));
-}
-
-void	ft_div_str_cpy(char *dest, char *src, int dig)
-{
-	int	i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (j < dig)
-	{
-		if (src[j] == '.')
-			j += 1;
-		dest[i] = src[j];
-		i += 1;
-		j += 1;
-	}
-}
-
-void	init_division(char *tab, char s[1000], char s_temp[1000], unsigned long div)
-{
-	int	n_dig;
-
-	ft_strncpy(tab, "0.", 2);
-	n_dig = nb_digit_f(div);
-	printf("n_dig = %d\n", n_dig);
-	if (n_dig == 1)
-	{
-		ft_div_str_cpy(s_temp, s, 3);
-		ft_strcat(tab, div_compute(s_temp, div));
-		s += 3;
-	}
-	else
-	{
-		ft_div_str_cpy(s_temp, s, n_dig + 1);
-		printf("s_temp init = |%s|\n", s_temp);
-		zero_filling(tab + 2, n_dig - 2);
-		ft_strcat(tab, div_compute(s_temp, div));
-		s += n_dig + 1;
-	}
-	ft_strcpy(s_temp, ft_itoa(ft_atoi(s_temp) * 10 + *s - '0'));
-}
-
-int		ft_nstrchr(char *s, char c)
-{
-	if (!s)
-		return (0);
-	while (*s)
-	{
-		if (*s != c)
+		if (*str > '0' && *str <= '9')
 			return (1);
-		s += 1;
+		str += 1;
 	}
 	return (0);
 }
 
-char	*ft_div_str(char s[1000], unsigned long div)
+char	*div_str(char *mantissa, __uint128_t div)
 {
-	int				i;
-	int				n_dig;
-	char			*tab;
-	char			s_temp[100];
+	char	*result;
+	__uint128_t		tmp;
 
-	printf("s_start = |%s|\n\n", s);
-	n_dig = nb_digit_f(div);
-	ft_bzero(s_temp, 100);
-	if (!(tab = (char *)malloc(1000)))
-		return (NULL);
-	ft_bzero(tab, 1000);
-	init_division(tab, s , s_temp, div);
-	s += (n_dig == 1 ? 4 : n_dig + 2);
-	while ((ft_atoi(s_temp) != 0) || ft_nstrchr(s, '0'))
+	int i;
+
+	i = 0;
+	tmp = 0;
+	result = NULL;
+	tmp = *mantissa - '0';
+//	printf("%lu\n", (unsigned long)div);
+	result = ft_strjoinf(result, ft_itoa(tmp / div), 1);
+	tmp = tmp % div;
+	if (*(mantissa + 1) == '.')
 	{
-		ft_strcat(tab, div_compute(s_temp, div));
-		ft_strcpy(s_temp, ft_itoa(ft_atoi(s_temp) * 10 + *s - '0'));
-		printf("%d = %d + %d\n", ft_atoi(s_temp) * 10 + *s - '0', ft_atoi(s_temp) * 10, *s - '0');
-		s += 1;
+		mantissa += 1;
+		result = ft_strjoinf(result, ".", 1);
 	}
-	return (tab);
+	mantissa += 1;
+	tmp = tmp * 10 + *mantissa - '0';
+	while ((tmp || (!tmp && is_number(mantissa))))
+	{
+		result = ft_strjoinf(result, ft_itoa(tmp / div), 1);
+//		printf("%d\n", (int)(tmp / div));
+//		printf("result : %s\n", result);
+		tmp = tmp % div;
+//		printf("%lu\n", (unsigned long)(tmp%div));
+//		printf("%lu\n", (unsigned long)tmp);
+		mantissa += 1;
+		tmp = tmp * 10 + *mantissa - '0';
+//		printf("%lu\n", (unsigned long)tmp);
+		
+		
+		
+//		printf("%.100lf\n", 0.5 / 70368744177664);
+//		printf("%s\n", mantissa);
+//		printf("i = %d\n\n", i);
+	}
+//	printf("result %s\n", result);
+	result = ft_strjoinf(result, "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", 1);
+	return (result);
 }
 
 int main()
 {
-	char tab[100];
-	unsigned long div;
+	double dbl;
 
-	div = 1 << 1;
-	printf("DIVISEUR : %lu\n", div);
-	zero_filling(tab, 99);
-	tab[99] = 0;
-	strncpy(tab, "1.5000000000000000000000000000000", 48);
-	printf("|%s|\n|0.75|\n\n",ft_div_str(tab, div));
+	dbl = 5.96958885555328886318626388756E-154;
+	printf("%.800lf\n", dbl);
 
-
-	div = 1 << 4;
-	printf("DIVISEUR : %lu\n", div);
-	zero_filling(tab, 99);
-	tab[99] = 0;
-	strncpy(tab, "1.5000000000000000000000000000000", 48);
-	printf("|%s|\n|0.09375|\n\n",ft_div_str(tab, div));
-
-
-	div = 1 << 8;
-	printf("DIVISEUR : %lu\n", div);
-	zero_filling(tab, 99);
-	tab[99] = 0;
-	strncpy(tab, "1.5000000000000000000000000000000", 48);
-	printf("|%s|\n|0.005859375|\n\n",ft_div_str(tab, div));
-
-
-	div = 1 << 10;
-	printf("DIVISEUR : %lu\n", div);
-	zero_filling(tab, 99);
-	tab[99] = 0;
-	strncpy(tab, "1.5000000000000000000000000000000", 48);
-	printf("|%s|\n|0.00146484375|\n\n",ft_div_str(tab, div));
-
-
-	div = 1 << 15;
-	printf("DIVISEUR : %lu\n", div);
-	zero_filling(tab, 99);
-	tab[99] = 0;
-	strncpy(tab, "1.5000000000000000000000000000000", 48);
-	printf("|%s|\n|0.0000457763671875|\n\n",ft_div_str(tab, div));
-
+	
 	return (0);
 }
